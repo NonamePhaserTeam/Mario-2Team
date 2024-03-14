95b3a7f323435628580d178c0845f71f0f23282f
ryzerrv2 <mariovicidomini7@gmail.com>
2024-03-14 17:22:14 +0100

    merge solved

--- src/main.ts
+++ src/main.ts
@@ -6,28 +6,29 @@ import Preloader from "./scenes/Preloader"
 import { gameSettings } from "./consts/GameSettings"
 
 const config: Phaser.Types.Core.GameConfig = {
-  type: Phaser.AUTO,
-  backgroundColor: gameSettings.bgColor,
-  parent: "my-game",
-  scale: {
-    mode: Phaser.Scale.FIT,
-    width: gameSettings.gameWidth,
-    height: gameSettings.gameHeight,
-    zoom: gameSettings.zoom
-  },
+	type: Phaser.AUTO,
+	backgroundColor: gameSettings.bgColor,
+	pixelArt: true,
+	parent: "my-game",
+	scale: {
+		mode: Phaser.Scale.FIT,
+		width: gameSettings.gameWidth,
+		height: gameSettings.gameHeight,
+		zoom: gameSettings.zoom
+	},
 
-  physics: {
-    default: "arcade",
-    arcade: { 
-      gravity: gameSettings.gravity,
-      debug: gameSettings.debug 
-    }
-  },
+	physics: {
+		default: "arcade",
+		arcade: { 
+		gravity: gameSettings.gravity,
+		debug: gameSettings.debug 
+		}
+	},
 
-  scene: [
-    Preloader,
-    HelloWorldScene,
-  ],
+	scene: [
+		Preloader,
+		HelloWorldScene,
+	],
 }
 
 export default new Phaser.Game(config)
