9a56215987db5cc8e525f84341962bd61135737e
Mellonnin <falciano.marco.25@gmail.com>
2024-03-29 12:03:50 +0100

    spawn piattaforme

--- src/consts/GameSettings.ts
+++ src/consts/GameSettings.ts
@@ -2,7 +2,7 @@ export let gameSettings: GameSettings = {
     gameWidth: window.innerWidth * window.devicePixelRatio,
     gameHeight: window.innerHeight * window.devicePixelRatio,
     bgColor: "#3498db", // celestra scuro
-    gravity: { x: 0, y: 900 },
+    gravity: { x: 0, y: 800 },
     debug: true,
     zoom: 1,
 };
--- src/scenes/Gioco_prova.ts
+++ src/scenes/Gioco_prova.ts
@@ -6,7 +6,6 @@ import TextureKeys from "../consts/TextureKeys";
 
 export default class Gioco_prova extends Phaser.Scene {
     player: Phaser.Physics.Arcade.Sprite;
-    platform: Phaser.Physics.Arcade.StaticGroup;
     platforms: Phaser.Physics.Arcade.StaticGroup;
     camera: Phaser.Cameras.Scene2D.Camera;
     W: Phaser.Input.Keyboard.Key;
@@ -15,9 +14,7 @@ export default class Gioco_prova extends Phaser.Scene {
     D: Phaser.Input.Keyboard.Key;
     SPACE: Phaser.Input.Keyboard.Key;
     playerSpeed = 500;
-    x_prova = 0
-    y_prova = gameSettings.gameHeight * 5 - 70
-    //background: capire quale namespace usare
+    y_piattaforme = gameSettings.gameHeight * 5 - 40
     constructor() { super(SceneKeys.Game); }
     init() {
         this.W = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W, true, false);
@@ -30,60 +27,80 @@ export default class Gioco_prova extends Phaser.Scene {
         this.camera.setBounds(
             0,
             0,
-            this.game.canvas.width / 4,
-            this.game.canvas.height * 5,
+            gameSettings.gameWidth / 4,
+            gameSettings.gameHeight * 5,
             true
         );
         this.physics.world.setBounds(
             gameSettings.gameWidth * 0.25,
             0,
-            this.game.canvas.width / 2,
-            this.game.canvas.height * 5
+            gameSettings.gameWidth / 2,
+            gameSettings.gameHeight * 5,
         );
     }
 
     create() {
-        this.cameras.main.setBackgroundColor(gameSettings.bgColor); //blu
+        this.camera.setBackgroundColor(gameSettings.bgColor); //blu
         this.platforms = this.physics.add.staticGroup();
-        for (let i = 0; i < 12; i++) {
+        for (let i = 0; i < 10; i++) {
             switch (i) {
-                case 1,4,7,10: // piattaforma singola
-                    this.CreatePlatform(0.5,2) 
-                case 2,5,8,11: //  piafforma centrale 2 mini mini piattaforme al lato
-                    this.CreatePlatform(0.3,0.4),  //left 
-                    this.CreatePlatform(0.5,0.75),  //middle 
-                    this.CreatePlatform(0.7,0.4)  //right 
-                case 3,6,9,12: // due piattaforme con spacco al centro
-                    this.CreatePlatform(0.3,0.8), //left
-                    this.CreatePlatform(0.7,0.8) //right
+                case 1, 6: // piattaforma singola
+                    this.CreatePlatform(0.5, 3)
+                    this.y_piattaforme -= gameSettings.gameHeight / 1.3
+                case 2, 7: //  piafforma centrale 2 mini mini piattaforme al lato
+                    this.CreatePlatform(0.29, 0.5)  //left 
+                    this.CreatePlatform(0.5, 1)  //middle 
+                    this.CreatePlatform(0.73, 0.5)  //right 
+                    this.y_piattaforme -= gameSettings.gameHeight / 1.3
+                case 3, 8: // due piattaforme con spacco al centro
+                    this.CreatePlatform(0.29, 1) //left
+                    this.CreatePlatform(0.73, 1) //right
+                    this.y_piattaforme -= gameSettings.gameHeight / 1.3
+                case 4, 9: // due piattaforme con spacco al a sinistra
+                    this.CreatePlatform(0.29, 0.8) //left
+                    this.CreatePlatform(0.73, 2.5) //right
+                    this.y_piattaforme -= gameSettings.gameHeight / 1.3
+                case 5, 10: // due piattaforme con spacco al a destra
+                    this.CreatePlatform(0.29, 2.5) //left
+                    this.CreatePlatform(0.73, 0.8) //right
+                    this.y_piattaforme -= gameSettings.gameHeight / 1.3
             }
         }
+
         this.player = this.physics.add
-            .sprite(gameSettings.gameWidth * 0.90, gameSettings.gameHeight * 5 - 70, TextureKeys.player)
+            .sprite(
+                gameSettings.gameWidth * 0.90,
+                gameSettings.gameHeight * 5 - 70,
+                TextureKeys.player
+            )
             .setBounce(1, 1)
             .setCollideWorldBounds(true)
             .setDrag(0.2, 0.2)
-            .setGravity(gameSettings.gravity.x, gameSettings.gravity.y)
-            .setScale(0.5);
+            .setGravity(
+                gameSettings.gravity.x,
+                gameSettings.gravity.y
+            )
+            .setScale(0.2);
 
         this.camera.startFollow(this.player, true, 1, 1);
-        this.physics.add.collider(this.player, this.platform);
-        this.physics.add.collider(this.player, this.platforms);
+        this.physics.add.collider(this.player, this.platforms)
     }
 
-    CreatePlatform(x_axis: number,scala_immagine: number) {
+
+    CreatePlatform(x_axis: number, scala_immagine: number) {
         this.platforms.create(
             gameSettings.gameWidth * x_axis,
-            this.y_prova,
+            this.y_piattaforme,
             'platform'
-        ).setScale(scala_immagine)
-        .body.updateFromGameObject();
-        this.y_prova -= gameSettings.gameHeight /1.3
+        ).setScale(scala_immagine, 1).body.updateFromGameObject();
+
     }
+
+
     update(time: number, delta: number): void {
         this.player.setVelocity(0);
         if (this.player.body.y <= gameSettings.gameHeight) {
-            this.player.setY(gameSettings.gameHeight * 10 - 70,)
+            this.player.setY(gameSettings.gameHeight * 5 - 70,)
         }
         if (this.A.isDown) {
             this.player.setFlipX(true); this.player.setVelocityX(-this.playerSpeed);
