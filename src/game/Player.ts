f6220e0189ca981470aa21c16c7ae1d499e351b3
Darkode <64909254+Darkxde@users.noreply.github.com>
2024-04-13 11:56:24 +0200

    player invisibile fixato

--- src/game/Player.ts
+++ src/game/Player.ts
@@ -21,16 +21,14 @@ export default class Player extends Phaser.Physics.Arcade.Sprite {
 	) {
 		super(scene, x, y, texture, frame);
 
-		this.scene.physics.world.enableBody(this);
-
-		// this.body.setSize(this.body.width * 0.4, this.body.height * 0.65);
-    	// this.body.setOffset(35, 40);
-
 		scene.physics.world.enable(this);
+
+		this.setCollideWorldBounds(true)
 		this.anims.play(AnimationKeys.Player.Idle);
 
 		this.scene.add.existing(this);
 
+		console.log("classe player")
 	}
 
 	create() {
